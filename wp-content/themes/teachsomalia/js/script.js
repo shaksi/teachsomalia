80bffa0fe11b10dc40eadf62e03170479c2f043c
Shaksi <iam@shak.si>
2012-06-30 13:31:35 +0100

    add teach somalia theme

---
+++ wp-content/themes/teachsomalia/js/script.js
@@ -0,0 +1,13 @@
+/* Author: Leonard Challis */
+
+$(document).ready(function(){
+
+  $('#slides').slides({
+    preload: true,
+    preloadImage: 'img/loading.gif',
+    play: 5000,
+    pause: 2500,
+    hoverPause: true
+  });
+  
+});
