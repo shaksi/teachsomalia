333e4bc5700ccb33cd76e8a3b2eec0867933a5e6
Shaksi <iam@shak.si>
2012-06-30 14:46:50 +0100

    update theme with header/footer/index

---
+++ wp-content/themes/teachsomalia/footer.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package WordPress
+ * @subpackage TeachSomalia
+ */
+?>
+<footer class="clearfix">
+  <div id="about">
+    <h4>About Teach Somalia</h4>
+    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+  </div>
+
+  <div id="contact">
+    <h4>Contact Teach Somalia</h4>
+    <p>
+      Teach Somalia<br />
+      6th Floor, Bridge House<br />
+      48-52 Baldwin Street<br />
+      Bristol BS1 1QB<br />
+      United Kingdom
+    </p>
+
+    <p>
+      tel: +44 (0)117 915 000<br />
+      fax: +44 (0)117 915 9630<br />
+      email: <a href="mailto:info@teachsomalia.com">info@teachsomalia.com</a>
+    </p>
+  </div>
+
+  <div id="links">
+    <ul>
+      <li><a href="#"><img src="./img/moxie.jpg" /></a></li>
+      <li><a href="#"><img src="./img/lithium.jpg" /></a></li>
+      <li><a href="#"><img src="./img/sas.jpg" /></a></li>
+      <li><a href="#"><img src="img/broadvision.jpg" /></a></li>
+      <li><img src="img/links-ribbon.png" /></li>
+    </ul>
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="#">HOME</a></li>
+      <li><a href="#">ABOUT US</a></li>
+      <li><a href="#">OUR WORK</a></li>
+      <li><a href="#">CONTACT</a></li>
+    </ul>
+
+    <p>Copyright United for Teach Somalia 2012. All rights reserved.</p>
+  </nav>
+</footer>
+</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+
+<script src="js/plugins.js"></script>
+<script src="js/script.js"></script>
+<script>
+var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+
+</body>
+</html>
---
+++ wp-content/themes/teachsomalia/header.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage TeachSomalia
+ */
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <title><?php
+  	/*
+  	 * Print the <title> tag based on what is being viewed.
+  	 */
+  	global $page, $paged;
+
+  	wp_title( '|', true, 'right' );
+
+  	// Add the blog name.
+  	bloginfo( 'name' );
+
+  	// Add the blog description for the home/front page.
+  	$site_description = get_bloginfo( 'description', 'display' );
+  	if ( $site_description && ( is_home() || is_front_page() ) )
+  		echo " | $site_description";
+
+  	// Add a page number if necessary:
+  	if ( $paged >= 2 || $page >= 2 )
+  		echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
+
+  	?></title>
+  <link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  <link rel="profile" href="http://gmpg.org/xfn/11" />
+  <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta name="viewport" content="width=device-width">
+  <!--[if lt IE 9]>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+  <![endif]-->
+	<script src="js/libs/modernizr-2.5.3.min.js"></script>
+  <?php
+  	/* We add some JavaScript to pages with the comment form
+  	 * to support sites with threaded comments (when in use).
+  	 */
+  	if ( is_singular() && get_option( 'thread_comments' ) )
+  		wp_enqueue_script( 'comment-reply' );
+
+  	/* Always have wp_head() just before the closing </head>
+  	 * tag of your theme, or you will break many plugins, which
+  	 * generally use this hook to add elements to <head> such
+  	 * as styles, scripts, and meta tags.
+  	 */
+  	wp_head();
+  ?>
+</head>
+<body>
+<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+
+<header>
+  <nav id="main">
+    <ul>
+      <li class="one"></li>
+      <li class="two"></li>
+      <li class="three"></li>
+      <li class="four"></li>
+      <li class="five">
+        <a href="#">HOME</a>
+      </li>
+      <li class="six">
+        <a href="#">CAMPAIGN</a>
+      </li>
+      <li class="seven">
+        <a href="#">JOIN MOVEMENT</a>
+      </li>
+      <li class="eight">
+        <a href="#">CONTACT</a>
+      </li>
+    </ul>
+  </nav>
+
+  <section>
+    <a href="/" id="logo">
+      <img src="img/logo.png">
+      <span class="title">TEACH SOMALIA</span>
+      <span class="sub-title">EDUCATING THE YOUTH</span>
+    </a>
+  </section>
+
+  <nav id="sub">
+    <ul>
+      <li><a href="#">TORONTO<span>CANADA</span></a></li>
+      <li><a href="#">LONDON<span>ENGLAND</span></a></li>
+      <li><a href="#">PARIS<span>FRANCE</span></a></li>
+      <li><a href="#">ROME<span>ITALY</span></a></li>
+      <li><a href="#">BERLIN<span>GERMANY</span></a></li>
+    </ul>
+  </nav>
+</header>
+<section id="slideshow">
+  <div id="slides-wrapper">
+    <div id="slides">
+      <div class="slides_container">
+        <a href="http://www.flickr.com/photos/stephangeyer/3020487807/" title="" target="_blank"><img src="img/slide-2.jpg" width="570" height="270" alt="Slide 2"></a>
+        <div class="content-slide"><h2>Something something...</h2><p>Social business is increasingly...</p></div>
+        <a href="http://www.flickr.com/photos/childofwar/2984345060/" title="" target="_blank"><img src="img/slide-3.jpg" width="570" height="270" alt="Slide 3"></a>
+        <a href="http://www.flickr.com/photos/b-tal/117037943/" title="" target="_blank"><img src="img/slide-4.jpg" width="570" height="270" alt="Slide 4"></a>
+        <a href="http://www.flickr.com/photos/bu7amd/3447416780/" title="" target="_blank"><img src="img/slide-5.jpg" width="570" height="270" alt="Slide 5"></a>
+      </div>
+      <a href="#" class="prev"><img src="img/arrow-prev.png" width="24" height="43" alt="Arrow Prev"></a>
+      <a href="#" class="next"><img src="img/arrow-next.png" width="24" height="43" alt="Arrow Next"></a>
+    </div>
+    <img src="img/example-frame.png" width="739" height="341" alt="Example Frame" id="frame">
+  </div>
+</section>
+
+<div id="content-wrapper">
